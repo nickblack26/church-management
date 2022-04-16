1264c483200946cb89cec53590e9adb378f01958
Nick Black <nicholas.black98@icloud.com>
2022-04-16 12:05:24 -0500

    added a bulk upload function for later use

--- bulkUpload.tsx
+++ bulkUpload.tsx
@@ -1,34 +1,18 @@
 import { addDoc, collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from './firebase';
+import { Member } from './typings';
 let json = require('/Users/Nick/Developer/church-management/members.json');
 
 const setData = async () => {
 	const churchRef = doc(db, 'churches', 'v7e7DRo9JTtUUqPp3ODW');
 
-	const callFb = async (
-		memberInfoStuff: Object,
-		memberAddress: Object,
-		memberEmail: Object,
-		memberPhone: Object
-	) => {
-		const memberInfo = await addDoc(
-			collection(churchRef, 'members'),
-			memberInfoStuff
-		);
-
-		const memberRef = doc(churchRef, 'members', memberInfo.id);
-
-		await addDoc(collection(memberRef, 'addresses'), memberAddress);
-
-		await addDoc(collection(memberRef, 'emails'), memberEmail);
-
-		await addDoc(collection(memberRef, 'numbers'), memberPhone);
+	const callFb = async (memberInfoStuff: Object) => {
+		await addDoc(collection(churchRef, 'members'), memberInfoStuff);
 	};
 
-	json.forEach((item, index) => {
+	json.forEach((item, index: number) => {
 		setTimeout(() => {
-			console.log(item);
-			const memberInfoStuff = {
+			const memberInfoStuff: Member = {
 				active_user: true,
 				background_check_clear: item.background_check_clear,
 				birthdate: new Date(item.birthdate),
@@ -42,51 +26,33 @@ const setData = async () => {
 				membership_status: item.membership_status,
 				prefix: item.prefix,
 				primary_campus: item.primary_campus,
+				addresses: [
+					{
+						city: item.addresses[0].city,
+						primary: item.addresses[0].primary,
+						state: item.addresses[0].state,
+						street: item.addresses[0].street,
+						type: item.addresses[0].type,
+						zip: item.addresses[0].zip,
+					},
+				],
+				numbers: [
+					{
+						number: item.numbers[0].number,
+						primary: item.numbers[0].primary,
+						type: item.numbers[0].type,
+					},
+				],
+				emails: [
+					{
+						address: item.emails[0].address,
+						primary: item.emails[0].primary,
+						type: item.emails[0].type,
+					},
+				],
 			};
 
-			const memberAddress = {
-				city: '',
-				primary: true,
-				state: '',
-				street: '',
-				type: '',
-				zip: 76123,
-			};
-
-			const memberPhone = {
-				number: '',
-				primary: true,
-				type: 'Mobile',
-			};
-
-			const memberEmail = {
-				address: '',
-				primary: true,
-				type: 'Home',
-			};
-
-			item.addresses.map((address) => {
-				memberAddress.city = address.city;
-				memberAddress.primary = address.primary;
-				memberAddress.state = address.state;
-				memberAddress.street = address.street;
-				memberAddress.type = address.type;
-				memberAddress.zip = address.zip;
-			});
-
-			item.numbers.map((number) => {
-				memberPhone.number = number.number;
-				memberPhone.primary = number.primary;
-				memberPhone.type = number.type;
-			});
-
-			item.emails.map((email) => {
-				memberEmail.address = email.address;
-				memberEmail.primary = email.primary;
-				memberEmail.type = email.type;
-			});
-
-			callFb(memberInfoStuff, memberAddress, memberEmail, memberPhone);
+			callFb(memberInfoStuff);
 		}, index * 1000);
 	});
 };
