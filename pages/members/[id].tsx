6c707345e206ec146930b73529c88fdf65fdbab3
Nick Black <nicholas.black98@icloud.com>
2022-04-16 12:07:34 -0500

    updated database calls to firebase

--- pages/members/[id].tsx
+++ pages/members/[id].tsx
@@ -1,103 +1,74 @@
-import { ObjectId } from 'mongodb';
-import { connectToDatabase } from '../../lib/mongodb';
 import Image from 'next/image';
-import { Campus, Member } from '../../typings';
-import styles from '../../styles/MemberPage.module.css';
+import { Member } from '../../typings';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
 
 interface Props {
+	id: string;
 	member: Member;
-	primary_campus: Campus;
 }
 
 export const getServerSideProps = async (context) => {
-	const { db } = await connectToDatabase();
 	const { id } = context.params;
-
-	const member: Member = await db
-		.collection('members')
-		.findOne({ _id: new ObjectId(id) });
-
-	const primaryCampus: Campus = await db
-		.collection('campuses')
-		.findOne({ _id: new ObjectId(member.primary_campus) });
+	const churchRef = doc(db, 'churches', 'v7e7DRo9JTtUUqPp3ODW');
+	const memberRef = doc(churchRef, 'members', id);
+	const memberInfo = await getDoc(memberRef);
+	const member = memberInfo.data();
 
 	return {
 		props: {
 			member: JSON.parse(JSON.stringify(member)),
-			primary_campus: JSON.parse(JSON.stringify(primaryCampus)),
 		},
 	};
 };
 
-const MemberPage = ({ member, primary_campus }: Props) => {
+const MemberPage = ({ id, member }: Props) => {
+	console.log(id);
+	const {
+		image,
+		full_name,
+		membership_status,
+		emails,
+		numbers,
+		addresses,
+		first_name,
+	} = member;
 	return (
 		<div>
 			<header>
 				<div>
-					<Image src={member.image} height={75} width={75} />
+					{image ? (
+						<Image src={image} height={75} width={75} />
+					) : (
+						<div
+							style={{
+								display: 'inline-block',
+								backgroundColor: 'gray',
+								padding: '1rem',
+								borderRadius: '50%',
+								height: '25px',
+								width: '25px',
+								lineHeight: '25px',
+								textAlign: 'center',
+								fontWeight: 'bold',
+								color: 'white',
+							}}
+						>
+							{first_name[0]}
+						</div>
+					)}
+
 					<div>
-						{member.full_name}
+						{full_name}
 						<div></div>
 					</div>
 				</div>
 				<div>
-					<button>{member.membership_status}</button>
-					<button>{primary_campus.name}</button>
+					<button>{membership_status}</button>
 				</div>
 			</header>
-			<div style={{ marginTop: '15vh' }}>
-				<h4>Contact Information</h4>
-				{member.emails.map((email) => (
-					<div
-						key={email.address}
-						style={{ display: 'flex', alignItems: 'center' }}
-					>
-						<div>{email.type}</div>
-						<p key={email.address}>{email.address}</p>
-						{email.primary === true ? (
-							<div className={styles.star}></div>
-						) : null}
-					</div>
-				))}
-			</div>
 		</div>
 	);
 };
 
 export default MemberPage;
-
-// // This function gets called at build time
-// export async function getStaticPaths() {
-// 	// Call an external API endpoint to get posts
-// 	const { db } = await connectToDatabase();
-// 	const members = await db.collection('members').find({}).toArray();
-
-// 	// Get the paths we want to pre-render based on posts
-// 	const paths = members.map((member: Member) => ({
-// 		params: { id: member._id },
-// 	}));
-
-// 	// We'll pre-render only these paths at build time.
-// 	// { fallback: false } means other routes should 404.
-// 	return { paths, fallback: false };
-// }
-
-// export async function getStaticProps({ params }) {
-// 	const { db } = await connectToDatabase();
-// 	const { id } = params;
-
-// 	const member: Member = await db
-// 		.collection('members')
-// 		.findOne({ _id: new ObjectId(id) });
-
-// 	const primaryCampus: Campus = await db
-// 		.collection('campuses')
-// 		.findOne({ _id: new ObjectId(member.primary_campus) });
-
-// 	return {
-// 		props: {
-// 			member: JSON.parse(JSON.stringify(member)),
-// 			primary_campus: JSON.parse(JSON.stringify(primaryCampus)),
-// 		},
-// 	};
-// }
--- pages/members/index.tsx
+++ pages/members/index.tsx
@@ -12,32 +12,60 @@ import {
 import { db } from '../../firebase';
 
 const MembersPage = ({ members }: Members) => {
-	console.log(members);
 	return (
-		<div>
+		<div
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				gap: '1rem',
+				width: '100%',
+			}}
+		>
 			{members.map((member) => {
-				const { image, first_name, last_name, full_name, _id } = member;
-
+				const { image, first_name, last_name, id, numbers } = member;
+				const primaryNumber = numbers.filter(
+					(number) => number.primary === true
+				);
 				return (
-					<Link key={full_name} href={`/members/${_id}`}>
-						<a
-							style={{
-								display: 'flex',
-								width: '100%',
-								justifyContent: 'space-evenly',
-								alignItems: 'center',
-							}}
-						>
-							<Image
-								src={image}
-								style={{ borderRadius: '0.5rem' }}
-								width={40}
-								height={40}
-							/>
-							<p>{first_name}</p>
-							<p>{last_name}</p>
-						</a>
-					</Link>
+					<div key={id}>
+						<Link href={`/members/${id}`}>
+							<a
+								style={{
+									display: 'grid',
+									gridTemplateColumns: 'repeat(4, 25%)',
+								}}
+							>
+								{image ? (
+									<Image
+										src={image}
+										style={{ borderRadius: '0.5rem' }}
+										width={40}
+										height={40}
+									/>
+								) : (
+									<div
+										style={{
+											display: 'inline-block',
+											backgroundColor: 'gray',
+											padding: '1rem',
+											borderRadius: '50%',
+											height: '1.5rem',
+											width: '1.5rem',
+											lineHeight: '1.5rem',
+											textAlign: 'center',
+											fontWeight: 'bold',
+											color: 'white',
+										}}
+									>
+										{first_name[0]}
+									</div>
+								)}
+								<p>{first_name}</p>
+								<p>{last_name}</p>
+								<p>{primaryNumber[0].number}</p>
+							</a>
+						</Link>
+					</div>
 				);
 			})}
 		</div>
@@ -50,10 +78,8 @@ export async function getServerSideProps() {
 	const churchRef = doc(db, 'churches', 'v7e7DRo9JTtUUqPp3ODW');
 	const membersRef = collection(churchRef, 'members');
 	const q = query(membersRef, orderBy('last_name', 'asc'), limit(50));
-	const qSnap = await getDocs(q);
-	let members = [];
-	qSnap.forEach((member) => {
-		members = [...members, member.data()];
+	const members = (await getDocs(q)).docs.map((doc) => {
+		return { id: doc.id, ...doc.data() };
 	});
 
 	return {
